1afa296057c41f1549b32b1eaca238d14d8d1b2f
Joshua Hoogstraat <34964599+jhoogstraat@users.noreply.github.com>
2025-09-13 09:45:29 +0200

    Use —push in docker build command

--- files/build/build_push.sh
+++ files/build/build_push.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker buildx -f "$1.Dockerfile" --platform=linux/arm64 -t registry.hoogstraat.de/$1:latest .
-docker push registry.hoogstraat.de/$1:latest
+docker buildx -f "$1.Dockerfile" --platform=linux/arm64 -t registry.hoogstraat.de/$1:latest --push .
